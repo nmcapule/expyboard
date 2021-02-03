e783fdbd681d681d676f4891e08bad7f20391af3
Nathaniel M. Capule <nmcapule@gmail.com>
2021-02-03 16:07:52 +0700

    feat: Initial integration of EditorJS.

--- src/pages/workspace/PostEditor.svelte
+++ src/pages/workspace/PostEditor.svelte
@@ -1,9 +1,46 @@
 <script lang="ts">
+  import EditorJS from '@editorjs/editorjs';
+  import Header from '@editorjs/header';
+  import List from '@editorjs/list';
+  import { onDestroy, onMount } from 'svelte';
   import type { Post } from '../../models/post';
 
   export let post: Post;
+
+  let editor: EditorJS;
+
+  onMount(() => {
+    editor = new EditorJS({
+      holder: 'editor',
+      tools: {
+        header: Header,
+        list: List,
+      },
+      minHeight: 0,
+    });
+  });
+
+  onDestroy(() => {
+    editor.destroy();
+  });
 </script>
 
 <div class="post-editor {$$props.class}">
-  {JSON.stringify(post)}
+  <div class="editor-container" id="editor" />
 </div>
+
+<style lang="less">
+  .post-editor {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--color-light-marvel);
+  }
+
+  .editor-container {
+    width: 400px;
+    padding: 0 10px;
+    background-color: var(--color-white);
+    border-radius: 4px;
+  }
+</style>
